cd9a1e835cae23641329bf8467db905b8a1750a3
Annabelle Perbet <annabelleperbet@yahoo.fr>
2023-11-05 18:01:04 +0100

    Mise en place du code js afin de rendre dynamique la progression dans les cercles

--- script.js
+++ script.js
@@ -18,3 +18,36 @@ window.onload = () => {
     }, 20);
   });
 };
+
+// Récupération des différents éléments
+const progressCircles = document.querySelectorAll(".progress__circle");
+const progressText = document.querySelectorAll(".data__progress");
+
+progressCircles.forEach((progressCircle) => {
+  // Création des différentes variables
+  let percentage = progressCircle.getAttribute("data-value");
+  let color = progressCircle.getAttribute("data-stroke");
+  let radius = progressCircle.r.baseVal.value;
+  let circumference = radius * 2 * Math.PI;
+  let stroke = circumference - (circumference * percentage) / 100;
+  progressCircle.style.setProperty("--stroke-dashoffset", stroke);
+  progressCircle.style.setProperty("--stroke-dasharray", circumference);
+  progressCircle.style.setProperty("--stroke", color);
+  progressCircle.style.setProperty("--animation-time", `${percentage * 100}ms`);
+});
+
+progressText.forEach((text) => {
+  let data = text.getAttribute("data-value");
+  // Création de la variable progressValue
+  let progressValue = 0;
+  // La méthode setInterval() appelle à plusieurs reprises une fonction ou exécute un extrait de code, avec un délai fixe entre chaque appel.
+  let progressBar = setInterval(() => {
+    // Incrémentation de la variable progressValue
+    progressValue++;
+    text.textContent = `${progressValue}%`;
+    if (progressValue == data) {
+      // La méthode globale clearInterval() annule une action répétitive et chronométrée qui a été précédemment établie par un appel à setInterval().
+      clearInterval(progressBar);
+    }
+  }, 100);
+});
